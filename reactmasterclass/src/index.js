37427498708c49b96095c9d20117e2a939dba8c5
namminwoo-hub <100527953+minwoogramer@users.noreply.github.com>
2022-06-02 17:37:29 +0900

    Style components

--- reactmasterclass/src/index.js
+++ reactmasterclass/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const darkTheme = {
+  textColor:"whitesmoke",
+  backgroundColor: "#111",
+};
+const lightTheme = {
+  textColor:"#111",
+  backgroundColor: "whitesmoke",
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
